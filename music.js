6f29635cb2b3b63d89ae510b3369f7efcf80ae97
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2024-01-09 17:57:38 +0400

    Create music.js

---
+++ music.js
@@ -0,0 +1,22 @@
+let songs = [
+    {
+        name: 'Rainy Days',
+        path: 'musics/Rainy Days.mp3'
+    },
+    {
+        name: 'Blue',
+        path: 'musics/Blue.mp3'
+    },
+    {
+        name: 'Love Me Again',
+        path: 'musics/Love Me Again.mp3'
+    },
+    {
+        name: 'Slow Dancing',
+        path: 'musics/Slow Dancing.mp3'
+    },
+    {
+        name: 'For Us',
+        path: 'musics/For Us.mp3'
+    },
+]
